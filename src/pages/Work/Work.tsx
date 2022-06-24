3eb8839651a6858fb693537c093dbf6108595c0b
lunarcroissant <76130184+lunarcroissant@users.noreply.github.com>
2022-06-25 00:54:46 +0100

    FIX: Removed screen orientation lock js

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -17,7 +17,6 @@ const Work = ({ data }: IProps) => {
   const { mobileMenuVisible } = useContext(GlobalContext);
 
   const projectsArray = projects.projects;
-  window.screen.orientation.lock("portrait");
 
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(projectsArray.length);
